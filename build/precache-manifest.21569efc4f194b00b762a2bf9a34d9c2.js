0551cb2dd7026130184420a00f15b4b2beef4a4b
serhan uras <serhan.uras@gmail.com>
2020-03-10 17:49:24 +0000

    build for test

---
+++ build/precache-manifest.21569efc4f194b00b762a2bf9a34d9c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b53f047afddebf184b778b7f7d4094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d61a2802d332dbb2b13",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0480a19ecaaf8243f025",
+    "url": "/static/css/main.22bcdc2a.chunk.css"
+  },
+  {
+    "revision": "0d61a2802d332dbb2b13",
+    "url": "/static/js/2.25b500ec.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.25b500ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0480a19ecaaf8243f025",
+    "url": "/static/js/main.35a6866f.chunk.js"
+  },
+  {
+    "revision": "64bdf79e89a1ad2912d7",
+    "url": "/static/js/runtime-main.27902d29.js"
+  },
+  {
+    "revision": "7350e08cedbddbf08dd68c0d282f66d1",
+    "url": "/static/media/ottobo.7350e08c.png"
+  }
+]);
