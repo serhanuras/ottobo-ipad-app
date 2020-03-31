04a7cf9c7b861fdd80ff87056e8b06a4a61b2062
Mustafa Serhan Uras <suras@Mustafas-MacBook-Pro.local>
2020-03-31 22:33:27 +0100

    getURLParameterChanges

--- src/App.js
+++ src/App.js
@@ -34,12 +34,8 @@ class App extends Component {
   }
 
   getUrlParameter = name => {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-    var results = regex.exec(window.location.search);
-    return results === null
-      ? ""
-      : decodeURIComponent(results[1].replace(/\+/g, " "));
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
   };
 
   nextOrderHandler = () => {
